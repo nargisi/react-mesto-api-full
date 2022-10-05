16b2dcd6beef920fd1bb063aed8374eff521c1ea
Nargisi <st-ana2005@yandex.ru>
2022-10-05 23:24:29 +0300

    Change cookies

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -69,7 +69,8 @@ module.exports.login = (req, res, next) => {
           res.cookie('jwt', token, {
             maxAge: 3600000,
             httpOnly: true,
-            sameSite: true,
+            sameSite: 'none',
+            secure: true,
           });
           res.send({ data: user });
         });
